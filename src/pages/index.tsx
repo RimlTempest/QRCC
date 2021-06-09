ec6d9f2ff99eaf9ef650d8f13c8e3229ea8b4262
RimlTempest <riml.slime@gmail.com>
2021-06-10 03:42:51 +0900

    add: コンポーネント適用、qrReaderのlazyを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import React, { ReactElement, useState } from "react"
+import React, { ReactElement, useState } from "react";
 import QRCode from "qrcode.react";
-import QrReader from 'react-qr-reader'
-import Header from '../components/header';
+// import QrReader from 'react-qr-reader';
+// import QrReader from 'react-qr-scanner';
+import Layout from '../components/layout';
+import SEO from '../components/SEO';
 import TextArea from '../components/textArea';
 import SelectBox from '../components/selectBox';
 import ConfirmDialog from '../components/confirmDialog';
 import { useIntl } from "gatsby-plugin-intl";
+const QrReader = React.lazy(() => import("react-qr-reader"));
 
 type levelType = "L" | "M" | "Q" | "H" | undefined;
 
@@ -67,85 +70,86 @@ function Index(): ReactElement {
   };
 
   return (
-    <>
-      <Header />
-      <div className="container max-w-md mx-auto mt-10">
-        <div className="overflow-hidden">
-          <div className="flex justify-center mb-10">
-            <QRCode 
-              id="qrCode"
-              value={text} 
-              size={128}
-              bgColor={backgroundColor}
-              fgColor={foregroundColor}
-              level={level}
-            />
-          </div>
-          <div className="mb-4 ml-4 mr-4">
-            <TextArea 
-              htmlFor="qrText"
-              title={intl.formatMessage({ id: "qrCode.text" })}
-              type="text"
-              placeholder="例) https://www.riml.work"
-              onChange={textChange}
-            />
-            <TextArea 
-              htmlFor="bgColor"
-              title={intl.formatMessage({ id: "qrCode.backgroundColor" })}
-              type="text"
-              placeholder="#FFFFFF"
-              onChange={backgroundColorChange}
-            />
-            <TextArea 
-              htmlFor="fgColor"
-              title={intl.formatMessage({ id: "qrCode.foregroundColor" })}
-              type="text"
-              placeholder="#000000"
-              onChange={foregroundColorChange}
-            />
-            <SelectBox
-              htmlFor="select"
-              title={intl.formatMessage({ id: "qrCode.level" })}
-              value={level}
-              options={levels}
-              onChange={levelChange}
-            />
-            <div className="flex justify-center mt-10">
-              <button 
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-3"
-                onClick={downloadQRCode}
-              >
-                {intl.formatMessage({ id: "save" })}
-              </button>
-              <button 
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => openConfirmDialog(true)}
-              >
-                {intl.formatMessage({ id: "scan" })}
-              </button>
-              <ConfirmDialog
-                title={intl.formatMessage({ id: "dialog.title" })}
-                open={open}
-                onClose={() => openConfirmDialog(false)}
-              >
-                <div className="mb-5">
-                  <QrReader
-                    delay={300}
-                    onError={handleError}
-                    onScan={handleScan}
-                    style={{ width: '100%' }}
-                  />
-                </div>
-                <div>
-                  <span className="text-gray-700">{intl.formatMessage({ id: "dialog.detail" })}</span>
-                  <p className="text-gray-700 ml-3 mt-2">{scan}</p>
-                </div>
-              </ConfirmDialog>
-            </div>
+    <Layout>
+      <SEO 
+        title={intl.formatMessage({ id: "title" })}
+        description={intl.formatMessage({ id: "description" })} 
+      />
+      <div className="overflow-hidden">
+        <div className="flex justify-center mb-10">
+          <QRCode 
+            id="qrCode"
+            value={text} 
+            size={128}
+            bgColor={backgroundColor}
+            fgColor={foregroundColor}
+            level={level}
+          />
+        </div>
+        <div className="mb-4 ml-4 mr-4">
+          <TextArea 
+            htmlFor="qrText"
+            title={intl.formatMessage({ id: "qrCode.text" })}
+            type="text"
+            placeholder="例) https://www.riml.work"
+            onChange={textChange}
+          />
+          <TextArea 
+            htmlFor="bgColor"
+            title={intl.formatMessage({ id: "qrCode.backgroundColor" })}
+            type="text"
+            placeholder="#FFFFFF"
+            onChange={backgroundColorChange}
+          />
+          <TextArea 
+            htmlFor="fgColor"
+            title={intl.formatMessage({ id: "qrCode.foregroundColor" })}
+            type="text"
+            placeholder="#000000"
+            onChange={foregroundColorChange}
+          />
+          <SelectBox
+            htmlFor="select"
+            title={intl.formatMessage({ id: "qrCode.level" })}
+            value={level}
+            options={levels}
+            onChange={levelChange}
+          />
+          <div className="flex justify-center mt-10">
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-3"
+              onClick={downloadQRCode}
+            >
+              {intl.formatMessage({ id: "save" })}
+            </button>
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => openConfirmDialog(true)}
+            >
+              {intl.formatMessage({ id: "scan" })}
+            </button>
+            <ConfirmDialog
+              title={intl.formatMessage({ id: "dialog.title" })}
+              open={open}
+              onClose={() => openConfirmDialog(false)}
+            >
+              <div className="mb-5">
+                <QrReader
+                  delay={300}
+                  onError={handleError}
+                  onScan={handleScan}
+                  style={{ width: '100%' }}
+                />
+              </div>
+              <div>
+                <span className="text-gray-700">{intl.formatMessage({ id: "dialog.detail" })}</span>
+                <p className="text-gray-700 ml-3 mt-2">{scan}</p>
+              </div>
+            </ConfirmDialog>
           </div>
         </div>
       </div>
-    </>
+    </Layout>
   )
 }
 
